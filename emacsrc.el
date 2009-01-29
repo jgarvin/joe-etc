a8278450404777318208b73dd2005939a57609bc
prophet <prophet@spikeshell.(none)>
2009-01-29 16:56:24 -0500

    Added GNU Global support for browsing tags in C/C++ projects :D

--- emacsrc.el
+++ emacsrc.el
@@ -23,3 +23,31 @@
 ;; Make emacs use the normal clipboard
 (setq x-select-enable-clipboard t)
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+
+;; Function to generate tags with GNU Global
+;; from here: http://emacs-fu.blogspot.com/2009/01/navigating-through-source-code-using.html
+(defun djcb-gtags-create-or-update ()
+  "create or update the gnu global tag file"
+  (interactive)
+  (if (not (= 0 (call-process "global" nil nil nil " -p"))) ; tagfile doesn't exist?
+    (let ((olddir default-directory)
+          (topdir (read-directory-name  
+                    "gtags: top of source tree:" default-directory)))
+      (cd topdir)
+      (shell-command "gtags && echo 'created tagfile'")
+      (cd olddir)) ; restore   
+    ;;  tagfile already exists; update it
+    (shell-command "global -u && echo 'updated tagfile'")))
+
+;; Rebind the normal find tag functions to use the GNU global versions
+(add-hook 'gtags-mode-hook 
+  (lambda()
+    (local-set-key (kbd "M-.") 'gtags-find-tag)   ; find a tag, also M-.
+    (local-set-key (kbd "M-,") 'gtags-find-rtag)))  ; reverse tag
+
+;; Generate tags whenever we open a C/C++ source file
+(add-hook 'c-mode-common-hook
+  (lambda ()
+    (require 'gtags)
+    (gtags-mode t)
+    (djcb-gtags-create-or-update)))
