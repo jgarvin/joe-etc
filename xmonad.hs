9f703ba562073b534c2b942e41c423bae1db1573
prophet <prophet@spikeshell.(none)>
2009-02-08 21:03:28 -0500

    Removed trailing whitespace

--- xmonad.hs
+++ xmonad.hs
@@ -6,10 +6,10 @@
 --
 -- Normally, you'd only override those defaults you care about.
 --
- 
+
 import XMonad
 import System.Exit
- 
+
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
@@ -23,23 +23,23 @@ import XMonad.Util.EZConfig
 -- For sawfish'esq jump-or-exec functionality
 import XMonad.ManageHook
 import XMonad.Actions.WindowGo
- 
+
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
 myTerminal      = "gnome-terminal"
- 
+
 -- Width of the window border in pixels.
 --
 myBorderWidth   = 1
- 
+
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
 -- ("right alt"), which does not conflict with emacs keybindings. The
 -- "windows key" is usually mod4Mask.
 --
 myModMask       = mod4Mask
- 
+
 -- The mask for the numlock key. Numlock status is "masked" from the
 -- current modifier status, so the keybindings will work with numlock on or
 -- off. You may need to change this on some systems.
@@ -54,7 +54,7 @@ myModMask       = mod4Mask
 -- numlock status separately.
 --
 myNumlockMask   = mod2Mask
- 
+
 -- The default number of workspaces (virtual screens) and their names.
 -- By default we use numeric strings, but any string may be used as a
 -- workspace name. The number of workspaces is determined by the length
@@ -65,23 +65,23 @@ myNumlockMask   = mod2Mask
 -- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
 --
 myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
- 
+
 -- Border colors for unfocused and focused windows, respectively.
 --
 myNormalBorderColor  = "#dddddd"
 myFocusedBorderColor = "#ff0000"
- 
+
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
- 
+
     -- launch a terminal
     [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch emacs
     , ((modMask,  xK_e), runOrRaise "emacs-snapshot-gtk" (className =? "Emacs"))
- 
+
     -- launch firefox
     , ((modMask,  xK_f), runOrRaise "firefox" (className =? "Firefox"))
 
@@ -92,63 +92,63 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- launch xchat
     , ((modMask,  xK_x), runOrRaise "xchat" (className =? "Xchat"))
 
-    -- close focused window 
+    -- close focused window
     , ((modMask , xK_c     ), kill)
- 
+
      -- Rotate through the available layout algorithms
     , ((modMask,               xK_space ), sendMessage NextLayout)
- 
+
     --  Reset the layouts on the current workspace to default
     , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
- 
+
     -- Resize viewed windows to the correct size
     , ((modMask,               xK_n     ), refresh)
- 
+
     -- Move focus to the next window
     , ((modMask,               xK_Tab   ), windows W.focusDown)
- 
+
     -- Move focus to the next window
     , ((modMask,               xK_j     ), windows W.focusDown)
- 
+
     -- Move focus to the previous window
     , ((modMask,               xK_k     ), windows W.focusUp  )
- 
+
     -- Move focus to the master window
     , ((modMask,               xK_m     ), windows W.focusMaster  )
- 
+
     -- Swap the focused window and the master window
     , ((modMask,               xK_Return), windows W.swapMaster)
- 
+
     -- Swap the focused window with the next window
     , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown  )
- 
+
     -- Swap the focused window with the previous window
     , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp    )
- 
+
     -- Shrink the master area
     , ((modMask,               xK_h     ), sendMessage Shrink)
- 
+
     -- Expand the master area
     , ((modMask,               xK_l     ), sendMessage Expand)
- 
+
     -- Push window back into tiling
     , ((modMask,               xK_t     ), withFocused $ windows . W.sink)
- 
+
     -- Increment the number of windows in the master area
     , ((modMask              , xK_comma ), sendMessage (IncMasterN 1))
- 
+
     -- Deincrement the number of windows in the master area
     , ((modMask              , xK_period), sendMessage (IncMasterN (-1)))
- 
+
     -- Quit xmonad
     , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
- 
+
     -- Restart xmonad
     , ((modMask              , xK_q     ),
           broadcastMessage ReleaseResources >> restart "xmonad" True)
     ]
     ++
- 
+
     --
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
@@ -156,28 +156,28 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     [((m .|. modMask, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
- 
- 
+
+
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
 --
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
- 
+
     -- mod-button1, Set the window to floating mode and move by dragging
     [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
- 
+
     -- mod-button2, Raise the window to the top of the stack
     , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
- 
+
     -- mod-button3, Set the window to floating mode and resize by dragging
     , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))
- 
+
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
- 
+
 ------------------------------------------------------------------------
 -- Layouts:
- 
+
 -- You can specify and transform your layouts by modifying these values.
 -- If you change layout bindings be sure to use 'mod-shift-space' after
 -- restarting (with 'mod-q') to reset your layout state to the new
@@ -190,19 +190,19 @@ myLayout = tiled ||| Mirror tiled ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
- 
+
      -- The default number of windows in the master pane
      nmaster = 1
- 
+
      -- Default proportion of screen occupied by master pane
      ratio   = 1/2
- 
+
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
- 
+
 ------------------------------------------------------------------------
 -- Window rules:
- 
+
 -- Execute arbitrary actions and WindowSet manipulations when managing
 -- a new window. You can use this to, for example, always float a
 -- particular program, or have a client always appear on a particular
@@ -221,22 +221,22 @@ myManageHook = composeAll
     , title     =? "Brood War"      --> doFloat
     , resource  =? "desktop_window" --> doIgnore
     , resource  =? "kdesktop"       --> doIgnore ]
- 
+
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
- 
+
 ------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
- 
+
 -- Run xmonad with the settings you specify. No need to modify this.
 --
 main = xmonad defaults
- 
+
 -- A structure containing your configuration settings, overriding
--- fields in the default config. Any you don't override, will 
+-- fields in the default config. Any you don't override, will
 -- use the defaults defined in xmonad/XMonad/Config.hs
--- 
+--
 -- No need to modify this.
 --
 defaults = defaultConfig {
@@ -249,11 +249,11 @@ defaults = defaultConfig {
         workspaces         = myWorkspaces,
         normalBorderColor  = myNormalBorderColor,
         focusedBorderColor = myFocusedBorderColor,
- 
+
       -- key bindings
         keys               = myKeys,
         mouseBindings      = myMouseBindings,
- 
+
       -- hooks, layouts
         layoutHook         = smartBorders $ ewmhDesktopsLayout $ avoidStruts $ layoutHook defaultConfig,
         manageHook         = myManageHook <+> manageDocks <+> manageHook defaultConfig,
