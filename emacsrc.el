ec9447fb27b60ec3dd4b28b0ae162e861275df97
jgarvin <k04jg02@gmail.com>
2010-01-21 16:00:30 -0500

    Add text highlighting for .incl files

--- emacsrc.el
+++ emacsrc.el
@@ -262,6 +262,8 @@
 ;; Show matching parentheses
 (show-paren-mode 1)
 
+(setq auto-mode-alist (cons '("\\.incl$" . c-mode) auto-mode-alist))
+
 ;;-------------
 ;; Switch between source and header
 ;;------------
