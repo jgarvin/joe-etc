c2d7357daa8b1826637de37bb3e912ca53ea7aa8
jgarvin <k04jg02@gmail.com>
2010-12-12 22:04:06 -0600

    First version done, picks most recent logfile. Now need to add the file switching.

--- utils/flail.py
+++ utils/flail.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python
 
 import glob
+import sys
 import os
 import stat
 import os.path as op
+import argparse
 
-def time_files(file_list):
-    logfilepaths = glob.glob("/var/tmp/tlapp.*.log")
+app_description = ("Automatically finds the most recent log and "
+                   "monitors it with less.")
+
+parser = argparse.ArgumentParser(description=app_description)
+parser.add_argument('--user', metavar='USER', type=str,
+                    dest="user", help='Only open logs from USER.')
+parser.add_argument('--lock', dest='lock', action='store_true',
+                    default=False,
+                    help="Don't automatically move to most recent log over time.")
+parser.add_argument(metavar='APPLICATION', type=str,
+                    dest="application",
+                    help='Open logs for this application.')
+
+args = parser.parse_args()
+
+def get_log_files():
+    glob_prefix = "/var/tmp/tlapp.*"
+    if args.application:
+        glob_path = glob_prefix + args.application + "*.log"
+    else:
+        glob_path = glob_prefix + ".log"
+
+    logfilepaths = glob.glob(glob_path)
     logpairs = [(os.stat(f)[stat.ST_MTIME], f) for f in logfilepaths]
     logpairs.sort(key=lambda x: x[0], reverse=True)
+    return logpairs
+
+def monitor_file(f):
+    os.system("less +F " + f)
+
+monitor_file(get_log_files()[0][1])
+
+sys.exit(0)
