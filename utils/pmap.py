11e2e37350d27eb210650bd02972468e2b800495
Joseph Garvin <joseph.h.garvin@gmail.com>
2012-10-24 15:02:05 -0500

    Handle nested sequences correctly and groups almost correctly...

--- utils/pmap.py
+++ utils/pmap.py
@@ -20,6 +20,7 @@ class Obj(object): pass
 cur_decimal = None
 cur_template = None
 cur_field = None
+cur_stack = []
 templates = []
 cur_tag = ""
 
@@ -52,7 +53,7 @@ is_nullable = { ('none'      , False) : False,
                 ('increment' , True)  : True }
 
 field_names = [ "string", "uInt32", "uInt64", "decimal",
-                "int32", "int64", "length", "sequence" ]
+                "int32", "int64", "length", "sequence", "group" ]
 
 operator_names = [ "constant", "copy", "default", "delta", "increment" ]
 
@@ -72,11 +73,14 @@ def build_field(name, attributes):
 
 def StartElementHandler(name, attributes):
     global cur_tag, cur_template, cur_field, cur_decimal
+    global cur_stack
     if name == "template":
         cur_template = Obj()
         cur_template.id = attributes["id"]
         cur_template.fields = []
         cur_template.name = attributes["name"]
+        cur_template.optional = False
+        cur_stack.append(cur_template)
 
         template_id_field = Obj()
         template_id_field.operator = 'copy'
@@ -86,7 +90,7 @@ def StartElementHandler(name, attributes):
         cur_template.fields.append(template_id_field)        
     elif name in field_names:
         cur_field = build_field(name, attributes)
-        cur_template.fields.append(cur_field)
+        cur_stack[-1].fields.append(cur_field)
     elif name in operator_names:
         cur_field.operator = name
         if attributes.has_key("value"):
@@ -104,16 +108,34 @@ def StartElementHandler(name, attributes):
         cur_field = build_field(name, attributes)
         cur_field.ftype = "int64"
         cur_decimal.mantissa = cur_field
+    elif name == "sequence":
+        cur_field.fields = []
+        cur_stack.append(cur_field)
+    elif name == "group":
+        cur_field.fields = []
+        cur_stack.append(cur_field)
 
 def EndElementHandler(name):
-    global templates, cur_template
+    global templates, cur_template, cur_stack
     if name == "template":
         templates.append(cur_template)
+        cur_stack.pop()
     elif name == "sequence":
-        end_sequence = Obj()
-        end_sequence.ftype = "end_sequence"
-        end_sequence.name = ""
-        cur_template.fields.append(end_sequence)
+        # Sometimes sequences aren't given an explicit length in the
+        # XML, but it's presence in the bitstream regardless, so we
+        # patch it in here.
+        foundLengthField = False
+        for f in cur_stack[-1].fields:
+            if f.ftype == "length":
+                foundLengthField = True
+        if not foundLengthField:
+            # We put an @ sign in to indicate this is a generated name,
+            # don't think FIXFAST allows them in names.
+            lengthField = build_field("length", { "name" : "@ImplicitLength"})
+            cur_stack[-1].fields.insert(0, lengthField)
+        cur_stack.pop()
+    elif name == "group":
+        cur_stack.pop()
 
 parser.StartElementHandler = StartElementHandler
 parser.EndElementHandler = EndElementHandler
@@ -135,23 +157,18 @@ max_op_width = 0
 for op in operator_names:
     max_op_width = max(max_op_width, len(op))
 
-for t in templates:
-    print "==========================================="
-    print "Template: " + t.name
-    pmap_bits = 0
-    last_was_end_sequence = False
-    for field in t.fields:
-        if last_was_end_sequence:
-            last_was_end_sequence = False
-            print '------EndSequence------'
+def traverseFields(container, depth, pmap_counters):
+    pmap_counters.append(0)
+    for field in container.fields:
+        if field.ftype == "sequence" or field.ftype == "group":
+            format_str = "%%%ds" % max_field_name_width
+            print '\t'*depth,format_str % ("{{{{{{{{{{{{")
+            print '\t'*depth,format_str % (field.ftype[0].upper() + ":" + field.name)
+            traverseFields(field, depth+1, pmap_counters)
+            print '\t'*depth,format_str % ("}}}}}}}}}}}}")
+            continue
 
-        if field.ftype == "sequence":
-            print '-------Sequence-------'
-            pmap_bits = 0
-            continue
-        elif field.ftype == "end_sequence":
-            last_was_end_sequence = True
-            continue
+        print '\t'*depth,
 
         field_op = field.operator
 
@@ -164,12 +181,29 @@ for t in templates:
         format_str = "%%%ds %%%ds" % (max_field_name_width, max_op_width)
         print format_str % (field.name, field_op),
 
-        if pmap_required[(field_op, field.optional)]:
+        field_is_optional = field.optional
+        if field.ftype == "length":
+            field_is_optional = field_is_optional or container.optional
+
+        if pmap_required[(field_op, field_is_optional)]:
             format_str = " %8s"
-            print format_str % ("PMAP_%02d" % pmap_bits),  
-            pmap_bits += 1
+
+            if field.ftype == "length":
+                pmap_bit = pmap_counters[-2]
+                pmap_counters[-2] += 1
+            else:
+                pmap_bit = pmap_counters[-1]
+                pmap_counters[-1] += 1
+                
+            print format_str % ("PMAP_%02d" % pmap_bit),
         else:
             print " %8s" % ' ',
-        if is_nullable[(field_op, field.optional)]:
+        if is_nullable[(field_op, field_is_optional)]:
             print " Nullable",
         print
+    pmap_counters.pop()
+    
+for t in templates:
+    print "==========================================="
+    print "Template: " + t.name + " (" + str(t.id) + ")"
+    traverseFields(t, 0, [])
