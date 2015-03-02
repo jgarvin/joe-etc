f59531ada4968d99eb87b0213a9046f0ff8714f8
Joseph Garvin <joseph.h.garvin@gmail.com>
2015-03-02 14:21:20 -0600

    Make emacs understand mime types, so much better. Also auto revert folders.

--- emacs/dired-custom.el
+++ emacs/dired-custom.el
@@ -5,6 +5,7 @@
 
 (defun etc-dired-hook ()
   ;; (rename-buffer (etc-dired-name))
+  (auto-revert-mode)
   )
 
 (add-hook 'dired-mode-hook #'etc-dired-hook)
@@ -19,14 +20,24 @@
   (end-of-buffer)
   (dired-next-line -1))
 
+(defun dired-xdg-open-file ()
+  "Opens the current file in a Dired buffer."
+  (interactive)
+  (xdg-open-file (dired-get-file-for-visit)))
+
+(defun xdg-open-file (filename)
+  "xdg-opens the specified file."
+  (interactive "fFile to open: ")
+  (let ((process-connection-type nil))
+    (start-process "" nil "/usr/bin/xdg-open" filename)))
+
+(define-key dired-mode-map (kbd "e") 'dired-xdg-open-file)
+
+(setq image-dired-external-viewer "/usr/bin/xdg-open")
+
 ;; going all the way to the real/top bottom of buffer in dired
 ;; is almost never what you want.
 (define-key dired-mode-map
   (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
 (define-key dired-mode-map
   (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
-
-;; ;; make directory view auto-refresh
-;; (add-hook 'dired-mode-hook
-;; 	  (lambda ()
-;; 	    (auto-revert-mode)))
