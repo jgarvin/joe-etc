b0468c772e90c6f596f2e994e85595722a370d7e
Joseph Garvin <jgarvin@radix.trade>
2015-04-22 10:17:02 -0500

    More midnight mode cleaning

--- emacs/midnight-custom.el
+++ emacs/midnight-custom.el
@@ -5,3 +5,16 @@
 ;; This prevents trying to kill the startup emacsclient
 (add-to-list 'clean-buffer-list-kill-never-buffer-names (user-login-name))
 
+(dolist (i '("*Packages*"
+             "*Find*"
+             "*magit-diff*"
+             "*magit-log*"
+             "*Backtrace*"
+             "*grep*"
+             "*Completions*"))
+  (add-to-list 'clean-buffer-list-kill-never-buffer-names i))
+
+(add-to-list 'clean-buffer-list-kill-regexps "compile|.*?|.*")
+(add-to-list 'clean-buffer-list-kill-regexps "run|.*?|.*")
+
+
