55ddc9dae79af523842515a71cdab750d5cb5940
Joseph Garvin <joseph.h.garvin@gmail.com>
2015-05-31 15:27:15 -0500

    Fix sudo freezing

--- emacs/md-projectile.el
+++ emacs/md-projectile.el
@@ -1,6 +1,8 @@
 ;; projectile slows things down if you call (projectile-current-project-files)
 ;; constantly, so we cache the results
 (defvar md-projectile-files nil)
+;; projectile freezes sudo prompts if you query for this when they're up
+(defvar md-projectile-projects nil)
 
 (defun md-get-projectile-files-impl ()
   (condition-case nil
@@ -14,6 +16,7 @@
              ;; doing while minibuffer window is up annoys TRAMP sudo
              (not (active-minibuffer-window)))
     (let ((md-update-projectile-files t))
+      (setq md-projectile-projects (projectile-relevant-known-projects))
       (setq md-projectile-files (md-get-projectile-files-impl)))))
 
 (add-hook 'md-window-selection-hook #'md-update-projectile-files)
