8daaa751041d530439be25721ea1e062882da2d9
Joseph <jgarvin@radix.trade>
2017-10-03 11:53:55 -0500

    Added ability to push to Gerritt

--- emacs/magit-custom.el
+++ emacs/magit-custom.el
@@ -1,1 +1,10 @@
 (setq magit-diff-refine-hunk 'all)
+
+;; from: https://emacs.stackexchange.com/questions/19672/magit-gerrit-push-to-other-branch
+(defun magit-push-to-gerrit ()
+  (interactive)
+  (magit-git-command-topdir "git push origin HEAD:refs/for/master"))
+(magit-define-popup-action 'magit-push-popup
+  ?h
+  "Push to gerrit"
+  'magit-push-to-gerrit)
