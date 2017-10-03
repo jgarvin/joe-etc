d89f1b657c7fa65d869155271adaee37505b3837
Joseph <jgarvin@radix.trade>
2017-10-03 11:38:14 -0500

    Use the new native line numbering, way better

--- emacs/linum-custom.el
+++ emacs/linum-custom.el
@@ -1,23 +1,23 @@
-(load-file "~/etc/emacs/linum-relative-custom.el")
-(linum-relative-on)
-(with-eval-after-load "linum"
-  ;; set `linum-delay' so that linum uses `linum-schedule' to update linums.
-  (setq linum-delay t)
+;; before we had native line numbering
+(when (< emacs-major-version 26)
+  (load-file "~/etc/emacs/linum-relative-custom.el")
+  (linum-relative-on)
+  (with-eval-after-load "linum"
+    ;; set `linum-delay' so that linum uses `linum-schedule' to update linums.
+    (setq linum-delay t)
 
-  ;; create a new var to keep track of the current update timer.
-  (defvar-local my-linum-current-timer nil)
+    ;; create a new var to keep track of the current update timer.
+    (defvar-local my-linum-current-timer nil)
 
-  ;; rewrite linum-schedule so it waits for 1 second of idle time
-  ;; before updating, and so it only keeps one active idle timer going
-  (defun linum-schedule ()
-    (when (timerp my-linum-current-timer)
-      (cancel-timer my-linum-current-timer))
-    (setq my-linum-current-timer
-          (run-with-idle-timer 0.1 nil #'linum-update-current))))
+    ;; rewrite linum-schedule so it waits for 1 second of idle time
+    ;; before updating, and so it only keeps one active idle timer going
+    (defun linum-schedule ()
+      (when (timerp my-linum-current-timer)
+        (cancel-timer my-linum-current-timer))
+      (setq my-linum-current-timer
+            (run-with-idle-timer 0.1 nil #'linum-update-current))))
+  (global-linum-mode))
 
-
-;;(require 'linum-relative)
-(global-linum-mode)
-
-;; (global-relative-line-numbers-mode)
-;; (setq relative-line-numbers-motion-function #'vertical-motion)
+(when (>= emacs-major-version 26)
+  (global-display-line-numbers-mode)
+  (setq display-line-numbers-type 'visual))
