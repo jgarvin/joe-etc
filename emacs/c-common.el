34ccc648c1c87689bfeef0ed679f409071eb2d57
Joseph <jgarvin@radix.trade>
2017-10-04 12:22:34 -0500

    More failed attempts at indenting fixing

--- emacs/c-common.el
+++ emacs/c-common.el
@@ -122,6 +122,11 @@
   ;;         String arg2,
   ;;         int arg3)
   (c-set-offset 'arglist-intro '+)
+
+  ;; none of these help the enum problem
+  ;; (c-set-offset 'statement-cont 0)
+  ;; (c-set-offset 'brace-list-open 0)
+  ;; (c-set-offset 'brace-list-close 0)
   )
 
 (defun etc-toggle-namespace-indent ()
@@ -157,42 +162,42 @@
 ;;(global-font-lock-mode t)
 (setq font-lock-maximum-decoration t)
 
-(defun etc-c++-mode-hook ()
-  (font-lock-add-keywords
-   nil '(;; complete some fundamental keywords
-         ("\\<\\(void\\|unsigned\\|signed\\|char\\|short\\|bool\\|int\\|long\\|float\\|double\\)\\>" . font-lock-keyword-face)
-         ;; add the new C++11 keywords
-         ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" . font-lock-keyword-face)
-         ("\\<\\(char[0-9]+_t\\)\\>" . font-lock-keyword-face)
-         ;; PREPROCESSOR_CONSTANT
-         ("\\<[A-Z]+[A-Z_]+\\>" . font-lock-constant-face)
-         ;; hexadecimal numbers
-         ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
-         ;; integer/float/scientific numbers
-         ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([ulUL]+\\|[eE][\\-+]?[0-9]+\\)?\\>" . font-lock-constant-face)
-         ;; user-types (customize!)
-         ("\\<[A-Za-z_]+[A-Za-z_0-9]*_\\(t\\|type\\|ptr\\)\\>" . font-lock-type-face)
-         ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)
-         ("auto ?&? +\(\\w+\)" 1 'font-lock-variable-name-face)
-         )))
+;; (defun etc-c++-mode-hook ()
+;;   (font-lock-add-keywords
+;;    nil '(;; complete some fundamental keywords
+;;          ("\\<\\(void\\|unsigned\\|signed\\|char\\|short\\|bool\\|int\\|long\\|float\\|double\\)\\>" . font-lock-keyword-face)
+;;          ;; add the new C++11 keywords
+;;          ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" . font-lock-keyword-face)
+;;          ("\\<\\(char[0-9]+_t\\)\\>" . font-lock-keyword-face)
+;;          ;; PREPROCESSOR_CONSTANT
+;;          ("\\<[A-Z]+[A-Z_]+\\>" . font-lock-constant-face)
+;;          ;; hexadecimal numbers
+;;          ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
+;;          ;; integer/float/scientific numbers
+;;          ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([ulUL]+\\|[eE][\\-+]?[0-9]+\\)?\\>" . font-lock-constant-face)
+;;          ;; user-types (customize!)
+;;          ("\\<[A-Za-z_]+[A-Za-z_0-9]*_\\(t\\|type\\|ptr\\)\\>" . font-lock-type-face)
+;;          ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)
+;;          ("auto ?&? +\(\\w+\)" 1 'font-lock-variable-name-face)
+;;          )))
 
-(add-hook 'c++-mode-hook #'etc-c++-mode-hook)
+;; (add-hook 'c++-mode-hook #'etc-c++-mode-hook)
 
 
-(add-hook 'c-mode-common-hook 'etc-setup-c-common)
+;; (add-hook 'c-mode-common-hook 'etc-setup-c-common)
 
-(defadvice c-lineup-arglist (around my activate)
-  "Improve indentation of continued C++11 lambda function opened as argument."
-  (setq ad-return-value
-        (if (and (equal major-mode 'c++-mode)
-                 (ignore-errors
-                   (save-excursion
-                     (goto-char (c-langelem-pos langelem))
-                     ;; Detect "[...](" or "[...]{". preceded by "," or "(",
-                     ;;   and with unclosed brace.
-                     (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
-            0                           ; no additional indent
-          ad-do-it)))                   ; default behavior
+;; (defadvice c-lineup-arglist (around my activate)
+;;   "Improve indentation of continued C++11 lambda function opened as argument."
+;;   (setq ad-return-value
+;;         (if (and (equal major-mode 'c++-mode)
+;;                  (ignore-errors
+;;                    (save-excursion
+;;                      (goto-char (c-langelem-pos langelem))
+;;                      ;; Detect "[...](" or "[...]{". preceded by "," or "(",
+;;                      ;;   and with unclosed brace.
+;;                      (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
+;;             0                           ; no additional indent
+;;           ad-do-it)))                   ; default behavior
 
 ;; This hack fixes indentation for C++11's "enum class" in Emacs.
 ;; http://stackoverflow.com/questions/6497374/emacs-cc-mode-indentation-problem-with-c0x-enum-class/6550361#6550361
@@ -203,7 +208,8 @@
       (goto-char pos)
       (up-list -1)
       (backward-sexp 1)
-      (looking-back "enum[ \t]+class[ \t]+[^}]+"))))
+      (or (looking-back "enum\\s-+class\\s-+")
+          (looking-back "enum\\s-+class\\s-+\\S-+\\s-*:\\s-*")))))
 
 (defun align-enum-class (langelem)
   (if (inside-class-enum-p (c-langelem-pos langelem))
