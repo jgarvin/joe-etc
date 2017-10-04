c8e9dded7b32d7e25696863c9a69763962acb61c
Joseph <jgarvin@radix.trade>
2017-10-04 12:21:37 -0500

    Enable e-mail sending

--- emacs/email-custom.el
+++ emacs/email-custom.el
@@ -7,6 +7,48 @@
 
 (setq gnus-use-full-window nil)
 
+(setq send-mail-function 'smtpmail-send-it)
+(setq smftpmail-smtp-service 587)
+(setq smtpmail-smtp-server "smtp.gmail.com")
+
+;; (require 'bbdb)
+;; (bbdb-initialize)
+;; (setq
+;;  bbdb-offer-save 1                        ;; 1 means save-without-asking
+
+;;  bbdb-use-pop-up t                        ;; allow popups for addresses
+;;  bbdb-electric-p t                        ;; be disposable with SPC
+;;  bbdb-popup-target-lines  1               ;; very small
+
+;;  bbdb-dwim-net-address-allow-redundancy t ;; always use full name
+;;  bbdb-quiet-about-name-mismatches 2       ;; show name-mismatches 2 secs
+
+;;  bbdb-always-add-address t                ;; add new addresses to existing...
+;;  ;; ...contacts automatically
+;;  bbdb-canonicalize-redundant-nets-p t     ;; x@foo.bar.cx => x@bar.cx
+
+;;  bbdb-completion-type nil                 ;; complete on anything
+
+;;  bbdb-complete-name-allow-cycling t       ;; cycle through matches
+;;  ;; this only works partially
+
+;;  bbbd-message-caching-enabled t           ;; be fast
+;;  bbdb-use-alternate-names t               ;; use AKA
+
+
+;;  bbdb-elided-display t                    ;; single-line addresses
+
+;;  ;; auto-create addresses from mail
+;;  bbdb/mail-auto-create-p 'bbdb-ignore-some-messages-hook
+;;  bbdb-ignore-some-messages-alist ;; don't ask about fake addresses
+;;  ;; NOTE: there can be only one entry per header (such as To, From)
+;;  ;; http://flex.ee.uec.ac.jp/texi/bbdb/bbdb_11.html
+
+;;  '(( "From" . "no.?reply\\|DAEMON\\|daemon\\|facebookmail\\|twitter"))
+;; )
+
+;; (add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus)
+
 ;; (defun etc-summary-mode-setup ()
 ;;   (gnus-summary-sort-by-date)
 ;;   (goto-char (point-max))
