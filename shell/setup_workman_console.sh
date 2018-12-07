7f078097a3bc65d47423bc1148e9d44d0dc7a2ac
Joseph Garvin <joseph.h.garvin@gmail.com>
2018-12-06 18:41:36 -0600

    workman setup

---
+++ shell/setup_workman_console.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -f /etc/vconsole.conf ]; then
+    echo "/etc/vconsole.conf already exists!" >2
+    exit 1
+fi
+
+mkdir -p /usr/local/share/kbd/keymaps/
+
+cp $(dirname "${BASH_SOURCE[0]}")/workman-p.iso15.kmap /usr/local/share/kbd/keymaps/
+
+echo "KEYMAP=/usr/local/share/kbd/keymaps/workmap-p.iso15.kmap" >> /etc/vconsole.conf
