cb5c41533effa48a0d6e44695ac9cfdc8b91f7a5
Joseph Garvin <joseph.h.garvin@gmail.com>
2024-05-09 16:30:04 -0500

    waybar

--- tarsnapper.sh
+++ tarsnapper.sh
@@ -8,4 +8,5 @@ source ~/opt/tarsnapper/bin/activate
 tarsnapper --target etc-\$date --sources ~/etc/  --deltas $DELTAS - make
 tarsnapper --target ssc-\$date --sources ~/ssc/  --deltas $DELTAS - make
 tarsnapper --target oldssc-\$date --sources ~/oldssc/  --deltas $DELTAS - make
+tarsnapper --target talon-settings-\$date --sources ~/.talon/user/  --deltas $DELTAS - make
 
---
+++ waybar_layout.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+env > /tmp/swaylog
+
+exec swaymsg -t get_workspaces | jq -r ".[] | select(.visible and .output == \"$WAYBAR_OUTPUT_NAME\") | .representation"
