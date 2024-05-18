842e20ad235b9758109e56570b6f94c396bc740e
Joseph Garvin <joseph.h.garvin@gmail.com>
2024-05-18 14:55:54 -0500

    backup obsidian

--- tarsnapper.sh
+++ tarsnapper.sh
@@ -9,4 +9,5 @@ tarsnapper --target etc-\$date --sources ~/etc/  --deltas $DELTAS - make
 tarsnapper --target ssc-\$date --sources ~/ssc/  --deltas $DELTAS - make
 tarsnapper --target oldssc-\$date --sources ~/oldssc/  --deltas $DELTAS - make
 tarsnapper --target talon-settings-\$date --sources ~/.talon/user/  --deltas $DELTAS - make
-
+tarsnapper --target obsidian-config-\$date --sources ~/.config/obsidian/  --deltas $DELTAS - make
+tarsnapper --target obsidian-data-\$date --sources ~/Documents/uber/  --deltas $DELTAS - make
