78d044e18be9dc0bcd2da32ee85a2c80b28f4a7c
Joseph Garvin <joseph.h.garvin@gmail.com>
2024-09-23 12:50:44 -0500

    progress

--- emacs/unit_layers.el
+++ emacs/unit_layers.el
@@ -1,7 +1,6 @@
-;; what does double tap correspond to, since it still needs to map to
-;; a key? prefix the whole thing with C-c?
-(global-set-key (kbd "C-SPC") #'etc-set-mark-or-expand-region)
+;; keep paste consistently available on the same key across all modes
 
+(global-set-key (kbd "C-M-y") #'yank)
 (define-key smartparens-mode-map (kbd "C-M-l") 'sp-beginning-of-sexp)
 (define-key smartparens-mode-map (kbd "C-M-/") 'sp-end-of-sexp)
 (define-key smartparens-mode-map (kbd "C-M-i") 'sp-next-sexp)
@@ -12,16 +11,17 @@
 (define-key smartparens-mode-map (kbd "C-M-o") 'sp-up-sexp)
 (define-key smartparens-mode-map (kbd "C-M-c") 'sp-unwrap-sexp)
 (define-key smartparens-mode-map (kbd "C-M-r") 'sp-rewrap-sexp)
-(define-key smartparens-mode-map (kbd "C-M-u") 'sp-forward-slurp-sexp)
-(define-key smartparens-mode-map (kbd "C-c C-M-u") 'sp-backward-slurp-sexp)
-(define-key smartparens-mode-map (kbd "C-M-p") 'sp-forward-barf-sexp)
-(define-key smartparens-mode-map (kbd "C-c C-M-p") 'sp-backward-barf-sexp)
+(define-key smartparens-mode-map (kbd "C-M-j") 'sp-forward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "C-c C-M-j") 'sp-backward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "C-M-'") 'sp-forward-barf-sexp)
+(define-key smartparens-mode-map (kbd "C-c C-M-'") 'sp-backward-barf-sexp)
+(define-key smartparens-mode-map (kbd "C-M-k") 'sp-kill-sexp)
+(define-key smartparens-mode-map (kbd "C-M-p") 'sp-split-sexp)
+(define-key smartparens-mode-map (kbd "C-M-/") 'sp-splice-sexp)
 
-;; do
+;; do I really want it all on one hand? so that you can more easily chain actions
 
 ;; have to decide what double taps key are
-(define-key smartparens-mode-map (kbd "C-M-j") 'sp-splice-sexp)
-(define-key smartparens-mode-map (kbd "C-M-i") 'sp-split-sexp)
 ;;(define-key smartparens-mode-map (kbd "C-S-u") 'sp-backward-up-sexp)
 ;;(define-key smartparens-mode-map (kbd "C-S-d") 'sp-backward-down-sexp)
 
