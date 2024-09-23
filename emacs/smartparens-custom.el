f1f4c425be3222b9e604388efb0b5493b575a9c9
Joseph Garvin <joseph.h.garvin@gmail.com>
2024-09-23 13:01:48 -0500

    done with sexp layer

--- emacs/smartparens-custom.el
+++ emacs/smartparens-custom.el
@@ -4,29 +4,7 @@
 
 (smartparens-global-mode 1)
 (show-smartparens-global-mode 1)
-
-(define-key smartparens-mode-map (kbd "C-M-f") 'sp-forward-sexp)
-(define-key smartparens-mode-map (kbd "C-M-b") 'sp-backward-sexp)
-(define-key smartparens-mode-map (kbd "C-M-d") 'sp-down-sexp)
-(define-key smartparens-mode-map (kbd "C-M-<down>") 'sp-down-sexp)
-(define-key smartparens-mode-map (kbd "C-S-d") 'sp-backward-down-sexp)
-(define-key smartparens-mode-map (kbd "C-M-o") 'sp-up-sexp)
-(define-key smartparens-mode-map (kbd "C-M-<up>") 'sp-up-sexp)
-(define-key smartparens-mode-map (kbd "C-S-u") 'sp-backward-up-sexp)
-(define-key smartparens-mode-map (kbd "C-M-u") 'sp-unwrap-sexp)
-(define-key smartparens-mode-map (kbd "C-M-n") 'sp-next-sexp)
-(define-key smartparens-mode-map (kbd "C-M-p") 'sp-previous-sexp)
-(define-key smartparens-mode-map (kbd "C-M-<home>") 'sp-beginning-of-sexp)
-(define-key smartparens-mode-map (kbd "C-M-<end>") 'sp-end-of-sexp)
-(define-key smartparens-mode-map (kbd "C-M-x") 'sp-transpose-sexp)
-(define-key smartparens-mode-map (kbd "C-M-y") 'sp-forward-slurp-sexp)
-(define-key smartparens-mode-map (kbd "C-M-g") 'sp-backward-slurp-sexp)
-(define-key smartparens-mode-map (kbd "C-M-v") 'sp-forward-barf-sexp)
-(define-key smartparens-mode-map (kbd "C-M-k") 'sp-backward-barf-sexp)
-(define-key smartparens-mode-map (kbd "C-M-j") 'sp-splice-sexp)
-(define-key smartparens-mode-map (kbd "C-M-i") 'sp-split-sexp)
-(define-key smartparens-mode-map (kbd "C-M-z") 'sp-rewrap-sexp)
-
+(sp-local-pair '(emacs-lisp-mode) "'" "'" :actions nil)
 ;; w/o this highlighting text and pressing parens doesn't wrap in C++
 ;; https://github.com/Fuco1/smartparens/issues/840
 (require 'cc-mode)
--- emacs/unit_layers.el
+++ emacs/unit_layers.el
@@ -1,27 +1,22 @@
 ;; keep paste consistently available on the same key across all modes
 
 (global-set-key (kbd "C-M-y") #'yank)
-(define-key smartparens-mode-map (kbd "C-M-l") 'sp-beginning-of-sexp)
-(define-key smartparens-mode-map (kbd "C-M-/") 'sp-end-of-sexp)
-(define-key smartparens-mode-map (kbd "C-M-i") 'sp-next-sexp)
-(define-key smartparens-mode-map (kbd "C-c C-M-i") 'sp-transpose-sexp)
-(define-key smartparens-mode-map (kbd "C-M-n") 'sp-previous-sexp)
-;;(define-key smartparens-mode-map (kbd "C-c C-M-n") 'sp-backward-transpose-sexp)
-(define-key smartparens-mode-map (kbd "C-M-u") 'sp-down-sexp)
-(define-key smartparens-mode-map (kbd "C-M-o") 'sp-up-sexp)
-(define-key smartparens-mode-map (kbd "C-M-c") 'sp-unwrap-sexp)
-(define-key smartparens-mode-map (kbd "C-M-r") 'sp-rewrap-sexp)
-(define-key smartparens-mode-map (kbd "C-M-j") 'sp-forward-slurp-sexp)
-(define-key smartparens-mode-map (kbd "C-c C-M-j") 'sp-backward-slurp-sexp)
-(define-key smartparens-mode-map (kbd "C-M-'") 'sp-forward-barf-sexp)
-(define-key smartparens-mode-map (kbd "C-c C-M-'") 'sp-backward-barf-sexp)
-(define-key smartparens-mode-map (kbd "C-M-k") 'sp-kill-sexp)
-(define-key smartparens-mode-map (kbd "C-M-p") 'sp-split-sexp)
-(define-key smartparens-mode-map (kbd "C-M-/") 'sp-splice-sexp)
 
-;; do I really want it all on one hand? so that you can more easily chain actions
-
-;; have to decide what double taps key are
-;;(define-key smartparens-mode-map (kbd "C-S-u") 'sp-backward-up-sexp)
-;;(define-key smartparens-mode-map (kbd "C-S-d") 'sp-backward-down-sexp)
-
+(define-key smartparens-mode-map (kbd "C-M-l") #'sp-beginning-of-sexp)
+(define-key smartparens-mode-map (kbd "C-M-/") #'sp-end-of-sexp)
+(define-key smartparens-mode-map (kbd "C-M-i") #'sp-next-sexp)
+(define-key smartparens-mode-map (kbd "C-c C-M-i") #'sp-transpose-sexp)
+(define-key smartparens-mode-map (kbd "C-M-n") #'sp-previous-sexp)
+;;(define-key smartparens-mode-map (kbd "C-c C-M-n") #'sp-backward-transpose-sexp)
+(define-key smartparens-mode-map (kbd "C-M-e") #'sp-down-sexp)
+(define-key smartparens-mode-map (kbd "C-M-o") #'sp-up-sexp)
+(define-key smartparens-mode-map (kbd "C-M-c") #'sp-unwrap-sexp)
+(define-key smartparens-mode-map (kbd "C-M-r") #'sp-rewrap-sexp)
+(define-key smartparens-mode-map (kbd "C-M-'") #'sp-forward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "C-c C-M-'") #'sp-backward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "C-M-,") #'sp-forward-barf-sexp)
+(define-key smartparens-mode-map (kbd "C-c C-M-,") #'sp-backward-barf-sexp)
+(define-key smartparens-mode-map (kbd "C-M-k") #'sp-kill-sexp) ;; wrong behavior
+(define-key smartparens-mode-map (kbd "C-M-p") #'sp-split-sexp)
+(define-key smartparens-mode-map (kbd "C-M-:") #'sp-splice-sexp)
+(define-key smartparens-mode-map (kbd "C-M-j") #'sp-copy-sexp) ;; wrong behavior
