e2025bc8dc24c25ef7aa34512dea6c8a99400ada
Joseph Garvin <joseph.h.garvin@gmail.com>
2024-10-02 13:42:40 -0500

    not perfect but closer to desired behavior

--- emacs/unit_layers.el
+++ emacs/unit_layers.el
@@ -1,14 +1,15 @@
+
 ;; modset-u is like a prefix argument, but without using the actual
 ;; emacs prefix mechanism so that is still available through a
 ;; conventional C-u
 (define-key smartparens-mode-map (kbd "C-M-l") #'sp-beginning-of-sexp)
 (define-key smartparens-mode-map (kbd "C-M-/") #'sp-end-of-sexp)
-(define-key smartparens-mode-map (kbd "C-M-n") #'sp-previous-sexp)
+(define-key smartparens-mode-map (kbd "C-M-n") #'etc-previous-sexp)
 (define-key smartparens-mode-map (kbd "C-M-u C-M-n") #'etc-backward-transpose-sexp)
-(define-key smartparens-mode-map (kbd "C-M-i") #'sp-next-sexp)
+(define-key smartparens-mode-map (kbd "C-M-i") #'etc-next-sexp)
 (define-key smartparens-mode-map (kbd "C-M-u C-M-i") #'etc-transpose-sexp)
 (define-key smartparens-mode-map (kbd "C-M-e") #'sp-down-sexp)
-(define-key smartparens-mode-map (kbd "C-M-o") #'sp-up-sexp)
+(define-key smartparens-mode-map (kbd "C-M-o") #'sp-backward-up-sexp)
 (define-key smartparens-mode-map (kbd "C-M-c") #'sp-unwrap-sexp)
 (define-key smartparens-mode-map (kbd "C-M-r") #'sp-rewrap-sexp)
 (define-key smartparens-mode-map (kbd "C-M-'") #'sp-forward-slurp-sexp)
@@ -23,7 +24,6 @@
 (define-key smartparens-mode-map (kbd "C-M-y") #'etc-duplicate-sexp)
 ;; (define-key smartparens-mode-map (kbd "C-M-SPC") #'sp-mark-sexp)
 
-;; my hand really hates the double taps
 ;; should duplicate apply to the containing sexp?
 ;; should mark apply to the containing sexp?
 ;; forward should go after then to beginning of next, likewise for backwards
@@ -31,12 +31,58 @@
 
 (defun debug-dump ()
   (interactive)
-  (message "%s" (buffer-substring (plist-get (sp-get-thing) :beg) (plist-get (sp-get-thing) :end))))
+  (message "\"%s\" -- \"%s\""
+           (buffer-substring (plist-get (sp-get-thing t) :beg) (plist-get (sp-get-thing t) :end))
+           (buffer-substring (plist-get (etc-sp-get-thing) :beg) (plist-get (etc-sp-get-thing) :end))))
 
 (global-set-key (kbd "C-M-z") #'debug-dump)
+(global-set-key (kbd "C-M-z") #'sp-previous-sexp)
+(defvar-local etc-travel-side t) ;; t is front, nil is back
+
+;; what happens if you press next or previous from the middle?
+;; if we wanted to be able to undo it, we would need to remember the jump which will suck
+
+(defun etc-next-sexp ()
+  (interactive)
+  (let* ((sexp (etc-sp-get-thing))
+         (beg (plist-get sexp :beg))
+         (end (plist-get sexp :end))
+         (original-point (point))
+         (travel-point (if etc-travel-side beg end))
+         (up-point (save-excursion (sp-backward-up-sexp) (point))))
+    (if (not (= travel-point (point)))
+        (goto-char travel-point)
+      (goto-char beg)
+      (sp-next-sexp)
+      (let* ((sexp (etc-sp-get-thing))
+             (beg (plist-get sexp :beg))
+             (end (plist-get sexp :end)))
+        (goto-char (if etc-travel-side beg end)))
+      (when (= (point) up-point)
+        (goto-char original-point)))))
+
+(defun etc-previous-sexp ()
+  (interactive)
+  (let* ((sexp (etc-sp-get-thing))
+         (beg (plist-get sexp :beg))
+         (end (plist-get sexp :end))
+         (original-point (point))
+         (travel-point (if etc-travel-side beg end))
+         (up-point (save-excursion (sp-backward-up-sexp) (point))))
+    (if (not (= travel-point (point)))
+        (goto-char travel-point)
+      (goto-char end)
+      (sp-previous-sexp)
+      (goto-char (1- (point)))
+      (let* ((sexp (etc-sp-get-thing))
+             (beg (plist-get sexp :beg))
+             (end (plist-get sexp :end)))
+        (goto-char (if etc-travel-side beg end)))
+      (when (= (point) up-point)
+        (goto-char original-point)))))
 
 (defun etc-sp-get-thing ()
-  (let ((sexp (sp-get-thing)))
+  (let ((sexp (sp-get-thing t)))
     (if (and (md-likely-followed-by-closer (1- (point)))
              (not (md-likely-preceded-by-opener (1+ (point)))))
         (save-excursion
