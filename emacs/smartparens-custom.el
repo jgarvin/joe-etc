ae4b9c0b02ed34a8fcc1ed7b035adb6b31e472e5
Joseph Garvin <joseph.h.garvin@gmail.com>
2024-11-01 13:09:34 -0500

    fix elisp backquote pairing
    smartparens is an elisp library why would it do this?

--- emacs/smartparens-custom.el
+++ emacs/smartparens-custom.el
@@ -5,6 +5,7 @@
 (smartparens-global-mode 1)
 (show-smartparens-global-mode 1)
 (sp-local-pair '(emacs-lisp-mode) "'" "'" :actions nil)
+(sp-local-pair '(emacs-lisp-mode) "`" "`" :actions nil)
 ;; w/o this highlighting text and pressing parens doesn't wrap in C++
 ;; https://github.com/Fuco1/smartparens/issues/840
 (require 'cc-mode)
