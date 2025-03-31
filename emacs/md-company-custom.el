6463310021a94b3a5ca4064520a117bd6c8109aa
Joseph Garvin <joseph.h.garvin@gmail.com>
2025-03-31 15:55:56 -0500

    company: alternative up/down keys for popup

--- emacs/md-company-custom.el
+++ emacs/md-company-custom.el
@@ -8,3 +8,14 @@
 ;;   (company-mode 0))
 
 ;; (add-hook 'sh-mode-hook #'etc-disable-company)
+
+;; Disable arrow keys in LSP completion popups (via company-mode)
+(with-eval-after-load 'lsp-mode
+  (with-eval-after-load 'company
+    ;; Remove arrow key bindings from company-active-map
+    (define-key company-active-map [up] nil)
+    (define-key company-active-map [down] nil)
+
+    ;; Set alternative navigation keys
+    (define-key company-active-map (kbd "M-p") #'company-select-previous)
+    (define-key company-active-map (kbd "M-n") #'company-select-next)))
