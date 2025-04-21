8bea9ffe078f040fa7a8c65626194fa997ba7cae
Joseph Garvin <joseph.h.garvin@gmail.com>
2025-04-21 14:47:44 -0500

    gnome: add pop shell
    not actually using this

--- nix/gnome.nix
+++ nix/gnome.nix
@@ -5,4 +5,6 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [ gnomeExtensions.pop-shell ];
 }
