f8b1f5650646d220b8665ea442e8243fe57d386d
Joseph Garvin <joseph.h.garvin@gmail.com>
2025-06-18 16:39:52 -0500

    nix: make printing really work

--- nix/printing.nix
+++ nix/printing.nix
@@ -4,6 +4,18 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # install epson drivers
+  services.printing.drivers = with pkgs; [
+    epson-escpr
+    epson-escpr2
+    gutenprint
+    gutenprintBin
+  ];
+
+  # setup printing by nework
+  services.printing.browsing = true;
+  services.printing.defaultShared = true;
+
   # Enable discovery of network printers.
   # go to http://localhost:631 to configure
   services.avahi = {
